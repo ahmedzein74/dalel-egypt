1f50656d9108ea0faae266eee67fbf2bec7fc5b3
Ahmed Zein <120061135+ahmedzein74@users.noreply.github.com>
2024-07-24 20:34:54 +0300

    App colors

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,32 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesForgetPassword
+  /// assets/images/forgetPassword.png
+  static const String assetsImagesForgetPassword = "assets/images/forgetPassword.png";
+
+  /// Assets for assetsImagesOnBoarding1
+  /// assets/images/onBoarding1.png
+  static const String assetsImagesOnBoarding1 = "assets/images/onBoarding1.png";
+
+  /// Assets for assetsImagesOnBoarding2
+  /// assets/images/onBoarding2.png
+  static const String assetsImagesOnBoarding2 = "assets/images/onBoarding2.png";
+
+  /// Assets for assetsImagesOnBoarding3
+  /// assets/images/onBoarding3.png
+  static const String assetsImagesOnBoarding3 = "assets/images/onBoarding3.png";
+
+  /// Assets for assetsImagesSuccessOrder
+  /// assets/images/successOrder.png
+  static const String assetsImagesSuccessOrder = "assets/images/successOrder.png";
+
+  /// Assets for assetsImagesVector1
+  /// assets/images/vector1.svg
+  static const String assetsImagesVector1 = "assets/images/vector1.svg";
+
+  /// Assets for assetsImagesVector2
+  /// assets/images/vector2.svg
+  static const String assetsImagesVector2 = "assets/images/vector2.svg";
+}
+
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppCplpr {
+  static Color primaryColor = Color(0xffC49E85);
+  static Color offWhite = Color(0xffF8F4F9);
+  static Color deepBrawen = Color(0xff6B4B3E);
+  static Color deepgray = Color(0xff6F6460);
+  static Color black = Color(0xff333333);
+  static Color lightGray = Color(0xffB4B4B4);
+  static Color grey = Color(0xffC4C4C4);
+}
